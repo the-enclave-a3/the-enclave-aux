90352e4256b95196cbd68825ee588bfb965695de
Mokka <mokkamichi.99@gmail.com>
2024-05-12 23:56:30 +0200

    move arsenal macros to a better spot

--- addons/main/script_macros.hpp
+++ addons/main/script_macros.hpp
@@ -1,1 +1,7 @@
 #include "\z\mti\addons\main\script_macros.hpp"
+
+#define ARSENALTYPE_NONE 0
+#define ARSENALTYPE_COMMON 1
+#define ARSENALTYPE_ASSAULT 2
+#define ARSENALTYPE_HEAVYWEAPONS 3
+#define ARSENALTYPE_RECON 4
